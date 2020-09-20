e6f0e9974f6adffda2631dbc36f2446cddffd458
Vishal Sengar <vishalsengardtu@gmail.com>
2020-09-20 13:28:48 +0530

    drum kit Final commit

--- 01 javascript-drum-kit/index.js
+++ 01 javascript-drum-kit/index.js
@@ -1,11 +1,11 @@
-window.addEventListener("keydown", function (e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return; //stop the function if wrong key is pressed
   audio.currentTime = 0; //rewind to start
   audio.play();
   key.classList.add("playing");
-});
+}
 
 function removeTransition(e) {
   this.classList.remove("playing");
@@ -13,3 +13,4 @@ function removeTransition(e) {
 
 const keys = document.querySelectorAll(".key");
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
