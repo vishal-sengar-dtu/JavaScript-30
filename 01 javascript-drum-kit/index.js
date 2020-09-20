68eb4f39442631a3fe65df0767068b39223f8f65
Vishal Sengar <vishalsengardtu@gmail.com>
2020-09-20 13:10:10 +0530

    drum kit Sound and Transition functionality

---
+++ 01 javascript-drum-kit/index.js
@@ -0,0 +1,15 @@
+window.addEventListener("keydown", function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; //stop the function if wrong key is pressed
+  audio.currentTime = 0; //rewind to start
+  audio.play();
+  key.classList.add("playing");
+});
+
+function removeTransition(e) {
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
