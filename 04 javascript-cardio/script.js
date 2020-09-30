7b42155da57fce93223448d17607c064bc497902
Vishal Sengar <vishalsengardtu@gmail.com>
2020-09-30 10:32:07 +0530

    flexbox-gallery-panel

--- 04 javascript-cardio/script.js
+++ 04 javascript-cardio/script.js
@@ -81,11 +81,9 @@ const data = [
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
-const fifteen = inventors.filter((inventor) => {
-  if (inventor.year >= 1500 && inventor.year < 1600) {
-    return true;
-  }
-});
+const fifteen = inventors.filter((inventor) =>
+  inventor.year >= 1500 && inventor.year < 1600 ? true : false
+);
 console.table(fifteen);
 
 // Array.prototype.map()
@@ -102,9 +100,10 @@ console.table(ordered);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
-const totalYears = inventors.reduce((total, inventor) => {
-  return total + (inventor.passed - inventor.year);
-}, 0);
+const totalYears = inventors.reduce(
+  (total, inventor) => total + (inventor.passed - inventor.year),
+  0
+);
 console.log(totalYears);
 
 // 5. Sort the inventors by years lived
